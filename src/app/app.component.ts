a0aa91e624eb14bbf211e01be913ef878a552558
Yolshin <elshin195@gmail.com>
2021-09-12 22:48:17 +0700

    add form login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit  {
   title = 'admin-ygreens-ru';
   showFiller = false;
+
+  constructor(public userServer: UserService) { }
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { NavbarComponent } from './component/navbar/navbar.component';
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -14,16 +15,27 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+import { LoginComponent } from './component/login/login.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
 
     MatIconModule,
     MatSidenavModule,
@@ -31,6 +43,10 @@ import {MatListModule} from '@angular/material/list';
     MatToolbarModule,
     MatListModule,
     MatDividerModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
 
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
